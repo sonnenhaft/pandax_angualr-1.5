223b1034157c7307d2edc4b3a4d5c695e01da2f4
vitalyshalak <vitaly.shalak@intellectsoft.net>
2016-07-22 17:21:48 +0300

    feat(home): added login form.

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -2,9 +2,19 @@ class HomeController {
 
   constructor () {
 
+    this.signIn = true;
+    this.signUp = false;
     this.isCustomer = true;
     this.isProvider = false;
 
+  }
+
+  onSignUp (credentials) {
+    console.log(credentials);
+  }
+
+  onSignIn (credentials) {
+    console.log(credentials);
   }
 
 }
