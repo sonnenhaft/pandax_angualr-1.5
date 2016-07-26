37ebb73bc0c52bc9065aa025b1d3e3e18fe24be7
vitalyshalak <vitaly.shalak@intellectsoft.net>
2016-07-26 14:43:01 +0300

    feat(profile): added types of service provider to create profile form.

--- client/app/components/profile/create/profile.create.controller.js
+++ client/app/components/profile/create/profile.create.controller.js
@@ -9,6 +9,8 @@ class ProfileCreateController {
     this.isProvider = this.session.user.type === 'provider';
     this.email = this.session.user.email;
 
+    this.providers = this.types();
+
   }
 
   onReady (profile) {
@@ -17,6 +19,42 @@ class ProfileCreateController {
 
   validate (field) {
     console.log(field);
+  }
+
+  types () {
+    const types = [
+      {
+        name: 'Prime XX',
+        cost: 250,
+        desc: '',
+        img: '../../../../assets/images/services/prime_xx.png',
+        active: false
+      },
+      {
+        name: 'Prime X',
+        cost: 150,
+        desc: '',
+        img: '../../../../assets/images/services/prime.png',
+        active: false
+      },
+      {
+        name: 'Prime',
+        cost: 50,
+        desc: '',
+        img: '../../../../assets/images/services/prime.png',
+        active: false
+      }
+    ];
+
+    return types;
+  }
+
+  switchObjectActivity (arr, index) {
+    _.map(arr, (object, i) => object.active = index === i);
+  }
+
+  getActiveObject (arr) {
+    return _.find(arr, object => object.active === true);
   }
 
 }
