958bce3e0598ed6ae3aa521f590f81c6cc6f2d38
vitalyshalak <vitaly.shalak@intellectsoft.net>
2016-08-04 19:13:52 +0300

    feat(profile): added user photo to background.

--- client/app/components/profile/view/profile.view.controller.js
+++ client/app/components/profile/view/profile.view.controller.js
@@ -5,6 +5,16 @@ export default class ProfileViewController {
 
     _.assign(this, {Storage});
 
+    this.session = Storage.getObject('MINX');
+
+  }
+
+  $onInit () {
+    if (this.session.user.photo && this.session.user.photo.$ngfBlobUrl) {
+      this.photo = {
+        background: 'url(' + this.session.user.photo.$ngfBlobUrl + ') no-repeat fixed center'
+      };
+    }
   }
 
 }
