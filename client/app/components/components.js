93b1cd02b1a88e4197ed613be586b2c0c23a88e4
Artem Ryzhkov <artem.ryzhkov@intellectsoft.net>
2016-08-29 18:01:52 +0300

    feat(config, about): set Pandax stripe key for testing, remove unnecessary component

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,6 +1,5 @@
 import angular from 'angular';
 import Home from './home/home';
-import About from './about/about';
 import Main from './main/main';
 import Profile from './profile/profile';
 import ProfileCreate from './profile/create/profile.create';
@@ -13,7 +12,6 @@ import Password from './password/password';
 
 let componentModule = angular.module('app.components', [
   Home,
-  About,
   Main,
   Profile,
   ProfileCreate,
