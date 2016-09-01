e71468088fbad32728daa5b5a1257125e49c8d2c
Artem Ryzhkov <artem.ryzhkov@intellectsoft.net>
2016-09-01 18:29:50 +0300

    fix(photoFullPage): real phot size

--- client/app/components/order/manipulationEntertainers/photoFullPage/photoFullPage.controller.js
+++ client/app/components/order/manipulationEntertainers/photoFullPage/photoFullPage.controller.js
@@ -1,22 +1,54 @@
 class photoFullPageController {
 
-  constructor ($state, $mdDialog) {
+  constructor ($state, $mdDialog, $scope) {
      'ngInject';
 
      _.assign(this, {
         $state,
         $mdDialog,
+        $scope,
         len: 0
      	});
-     this.len = this.photos.length;
+
+    this.len = this.photos.length;
+
+/*
+  For future: add handlers for arrow keyup event
+    document.addEventListener("keyup", this.keyEventHandler.bind(this, true), false);
+
+    this.$scope.$on("$destroy", (_e) => {
+      document.removeEventListener("keyup", this.keyEventHandler);    // not working. Look at best way
+    });
+*/
+
   }
 
-  goToPhotoByIndex(direction) {
-  	let possibleIndex = this.photoIndexActive + direction;
+  goToPhotoByIndex(direction/*, byKeyboard = false*/) {
+    let possibleIndex = this.photoIndexActive + direction;
   	if (possibleIndex >= 0 && possibleIndex < this.len) {
 	  	this.photoIndexActive = possibleIndex;
+/*      if (byKeyboard) {       //  angular doesn't update value in the scope after keypress
+        this.$scope.$apply();
+      }*/
   	}
   }
+
+/*  keyEventHandler (byKeyboard = false, e) {
+    switch(e.which) {
+        case 37:              // left arrow
+            this.goToPhotoByIndex(-1, byKeyboard);
+            break;
+        case 40:              // bottom arrow
+            this.goToPhotoByIndex(-1, byKeyboard);
+            break;
+        case 38:               // top arrow
+            this.goToPhotoByIndex(+1, byKeyboard);
+            break;
+        case 39:               // right arrow
+            this.goToPhotoByIndex(+1, byKeyboard);
+            break;
+    }
+  }*/
 }
 
 export default photoFullPageController;
