41dbe024b3af724e224a066187adbf41cdf88635
Artem Ryzhkov <artem.ryzhkov@intellectsoft.net>
2016-09-06 15:23:08 +0300

    feat(confirmedEntertainers, webSocket): inject webService component, add component

--- client/app/components/order/manipulationEntertainers/confirmedEntertainers/confirmedEntertainers.controller.js
+++ client/app/components/order/manipulationEntertainers/confirmedEntertainers/confirmedEntertainers.controller.js
@@ -1,12 +1,20 @@
 class confirmedEntertainersController {
 
-  constructor (OrderService) {
+  constructor (OrderService, WebSocket) {
      'ngInject';
 
      _.assign(this, {
      		OrderService, 
+     		WebSocket,
      		entertainer: null
      	});
+  }
+
+  $onInit () {
+  	/*
+  		ToDo: add handler for events from server
+  	 */
+  	// this.WebSocket.invites();
   }
 
 }
--- client/app/components/order/manipulationEntertainers/confirmedEntertainers/confirmedEntertainers.js
+++ client/app/components/order/manipulationEntertainers/confirmedEntertainers/confirmedEntertainers.js
@@ -2,11 +2,13 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import confirmedEntertainersComponent from './confirmedEntertainers.component';
 import OrderService from '../../../../services/orderService/orderService';
+import WebSocket from '../../../../services/webSocket/webSocket';
 
 export default angular
   .module('confirmedEntertainers', [
     uiRouter,
-    OrderService
+    OrderService,
+    WebSocket
   ])
 
   .component('confirmedEntertainers', confirmedEntertainersComponent)
