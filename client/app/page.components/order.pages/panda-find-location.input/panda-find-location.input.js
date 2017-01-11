e0630d702782178c45623e1d5f62d876fd39442f
Vladimir Shein <vladimir.sonnenhaft@gmail.com>
2017-01-11 17:00:15 +0100

    addes scaffold for NY zip validation

--- client/app/page.components/order.pages/panda-find-location.input/panda-find-location.input.js
+++ client/app/page.components/order.pages/panda-find-location.input/panda-find-location.input.js
@@ -5,10 +5,10 @@ const UNKNOWN_PLACE = 'Unknown place';
 
 class controller {
 
-  constructor (GoogleGeoLocationService, Validation) {
+  constructor (GoogleGeoLocationService, $log) {
     'ngInject';
 
-    Object.assign(this, { GoogleGeoLocationService, Validation });
+    Object.assign(this, { GoogleGeoLocationService, $log });
     this.locationName = 'Loading current location...';
   }
 
@@ -43,20 +43,29 @@ class controller {
     this.ngModel.$setValidity('addressRegex', !this.addressRegex || !(this.locationName && new RegExp(this.locationName).test(this.locationName)));
     this.ngModel.$setValidity('required', !this.ngRequired || this.locationName);
     this.ngModel.$setValidity('recognizableAddress', !this.recognizableAddress || this.locationName !== UNKNOWN_PLACE);
+    const { location = {} } = (this.ngModel.$viewValue || { location: { address_components: [] } });
+    const { short_name: zip } = location.address_components.find(item => item.types.indexOf('postal_code') !== -1) || {};
+
+    this.ngModel.$setValidity('nycOnly', !this.nycOnly || (zip && this.validateBigAppleZip(zip)));
+  }
+
+  validateBigAppleZip (zip) {
+    this.$log.warn('TODO(vald): add zip validation', zip);
+    return true;
   }
 
   chooseLocation (location) {
     const { geometry: { location: coords }, formatted_address: locationName } = location;
 
     this.locationName = locationName;
-    this.validateModel( );
     this.ngModel.$setViewValue({ coords, location });
+    this.validateModel( );
   }
 
   clearLocation ( ) {
     this.locationName = '';
+    this.ngModel.$setViewValue( );
     this.validateModel( );
-    this.ngModel.$setViewValue(null);
   }
 }
 
@@ -64,7 +73,7 @@ export default angular.module('pandaFindLocationInput', [
   GoogleGeoLocationService
 ]).component('pandaFindLocationInput', {
   require: { ngModel: '^ngModel' },
-  bindings: { addressContains: '@', ngRequired: '<', recognizableAddress: '<' },
+  bindings: { addressContains: '@', ngRequired: '<', recognizableAddress: '<', nycOnly: '<' },
   template,
   controller
 }).name;
