96ec27779d8ee7aa8e5df4a4be2b0a58aa743629
Vladimir Shein <vladimir.sonnenhaft@gmail.com>
2017-04-28 16:35:09 +0200

    PANDAX-759 fixing mobile view on dob input

--- client/app/user.pages/profile.pages/date-of-birth.input.component/date-of-birth.input.component.js
+++ client/app/user.pages/profile.pages/date-of-birth.input.component/date-of-birth.input.component.js
@@ -158,4 +158,10 @@ export default angular.module('dateOfBirthInput', [
   bindings: { minAge: '<', asObject: '<', ngReadonly: '<' },
   controller,
   template
-}).name;
+}).directive('convertToNumber', ( ) => ({ // need for number inputs
+  require: 'ngModel',
+  link (i, j, k, { $parsers, $formatters }) {
+    $parsers.push(val => val != null ? parseInt(val, 10) : null); // eslint-disable-line no-confusing-arrow
+    $formatters.push(val => val != null ? `${val}` : null); // eslint-disable-line no-confusing-arrow
+  }
+})).name;
